0d8fb3e446244e7ea28def1bb32bc22a076f6044
jaytanetdev <jaytanetdev@gmail.com>
2026-01-04 23:38:32 +0700

    fix: import seo file

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from 'next';
-import { siteConfig } from './config/seo';
+import { siteConfig } from './utils/seo';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
